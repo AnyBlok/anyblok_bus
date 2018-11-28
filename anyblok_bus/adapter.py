6e953939d2eb78817de7af2ab58827c5c9d1c715
jssuzanne <jssuzanne@anybox.fr>
2018-11-28 18:59:45 +0100

    Refactored schema parameter (#13)
    * Refactored schema parameter
    
    Added adapter parameter to transform bus message, the schema attribute
    become now a simple kwargs argument give to adapter.
    
    The adapter is not required.
    
    To keep the compatibility, if no adapter is defined with a schema then
    the adapter is schema_adapter

---
+++ anyblok_bus/adapter.py
@@ -0,0 +1,13 @@
+# This file is a part of the AnyBlok / Bus api project
+#
+#    Copyright (C) 2018 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file,You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+from json import loads
+
+
+def schema_adapter(registry, body, schema=None, **kwargs):
+    schema.context['registry'] = registry
+    return schema.load(loads(body))
